a7b6b73c755002c179f71154187163351276f58f
Taruu <markov2002@bk.ru>
2018-06-20 08:22:16 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(607, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обновление";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Black;
+            this.progressBar1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(12, 405);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(589, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(12, 389);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ожидание";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(16, 320);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "Яндекс диск";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 343);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Наш сервер";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(12, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Выбор сервер для скачивания (рекомендуется Яндекс диск)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(697, 405);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Играть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(335, 363);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Выбор папки";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 366);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(422, 362);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Сохранить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(13, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(13, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(504, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(806, 456);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Net;
+using System.Windows.Forms;
+
+
+
+
+
+
+
+
+
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private string URL;
+        private string path;
+        private string pathDic;
+        private string DivGame = @"\tandots";
+        private string Version;
+
+        public Form1()
+        {
+            //действия при запуске
+            InitializeComponent();
+            path = Properties.Settings.Default.directory;
+            textBox1.Text = path;
+            URL = "http://www.tandots.ru/LA.zip";
+            Testdiv();
+            serverVer();
+            label3.Text = Properties.Settings.Default.ver;
+        }
+        //узнаем версию
+        private void UpdateVer()
+        {
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead("http://www.tandots.ru/ver.txt"))
+            using (var reader = new StreamReader(stream))
+            Version = reader.ReadToEnd();
+        }
+        //узнаем версию сервера
+        private void serverVer()
+        {
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead("http://www.tandots.ru/ver.txt"))
+            using (var reader = new StreamReader(stream))
+                label4.Text = "Версия сервера: " + reader.ReadToEnd();
+        }
+
+        //создание новой папки
+        private void Testdiv()
+        {
+            Directory.CreateDirectory(path + @"\tandots");
+
+        }
+        //прогрузка конфигов
+        private void InstConfigDiv()
+        {
+            path = Properties.Settings.Default.directory;
+            pathDic = @path;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            InstConfigDiv();
+            if (Directory.Exists(@pathDic + @DivGame + @"\.minecaft") & Directory.Exists(@pathDic + @DivGame + @"\Launcher") & Directory.Exists(@pathDic + @DivGame + @"\TLauncher") & File.Exists(@pathDic + @DivGame + @"tlauncher.args") & File.Exists(@pathDic + @DivGame + @"start.exe"))
+            {
+                MessageBox.Show("Все есть");
+            }
+            else
+            {
+                DownloadZip();
+            }
+        }
+        //скачивание
+        private void DownloadZip()
+        {
+            InstConfigDiv();
+
+            WebClient webClient = new WebClient();
+
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFileAsync(new Uri(URL), pathDic + @"\tandots" + @"\TL.exe");
+
+        }
+
+        void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label1.Text = "Загрузка "+Convert.ToString(progressBar1.Value)+"%";
+            
+        }
+
+        // действия после скачивание архива
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            label1.Text = "Распоковка";
+            Zip();
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void Zip()
+        {
+            InstConfigDiv();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = pathDic + DivGame + @"\TL.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = "";
+            Process.Start(startInfo);
+        }
+        private void Delite()
+        {
+            SortedSet<string> filesToIgnore = new SortedSet<string>(StringComparer.OrdinalIgnoreCase) {
+                "servers.dat",
+                "resourcepacks",
+                "saves",
+                "config"
+            };
+            foreach (var filePath in Directory.EnumerateFiles(path + DivGame + @"\.minecraft"))
+            {
+                string fileName = Path.GetFileName(filePath);
+                if (!filesToIgnore.Contains(fileName))
+                    File.Delete(filePath);
+            }
+
+            foreach (var dirPath in Directory.EnumerateDirectories(path + DivGame + @"\.minecraft"))
+            {
+                string DirName = Path.GetFileName(dirPath);
+                if (!filesToIgnore.Contains(DirName))
+                    Directory.Delete(@dirPath, true);
+            }
+
+            //Directory.GetDirectories
+
+        }
+
+
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            URL = "https://downloader.disk.yandex.ru/disk/d6f2da80cbb6a2b7b10366d2886d997c725a04b12e9eab2c4828908405e014d4/5b294070/PchyKDV67Ffgsy8bWN3BKg3kWRwoAHfFSCbze3jPe6QhtJ7K7R11-6iQeZVCFBmzdU36KCoie11gtLCH1Eim3w%3D%3D?uid=0&filename=LA.zip&disposition=attachment&hash=EukMpZERjc1%2BT4d4UCcmILyBpSaLY7we4lqIRRbRHs8%3D%3A&limit=0&content_type=application%2Fx-zip-compressed&fsize=331501514&hid=2ee0bb473f524c20965ddc45f9909225&media_type=compressed&tknv=v2";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            URL = "http://www.tandots.ru/LA.zip";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(FBD.SelectedPath);
+                path = FBD.SelectedPath;
+                textBox1.Text = FBD.SelectedPath;
+                path = textBox1.Text;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            path = @textBox1.Text;
+            Properties.Settings.Default.directory = textBox1.Text;
+            Properties.Settings.Default.Save();
+            MessageBox.Show("Сохранено");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Zip();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Delite();
+        }
+    }
+
+}
